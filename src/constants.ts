6262059de052d85bd6e6c3a9bac296dc2322c01a
Peter van Gulik <p.w.van.gulik@gmail.com>
2018-10-29 22:14:09 +0100

    Update salesforce extensions to latest version. Update version tag. Update packaging script to minimize package size.

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@
  * Default constants used in this extensipn
  */
 export default class Constants {
-    public static VERSION = '0.3.0';
+    public static VERSION = '0.3.1';
     public static CONFIG_SECTION = 'vlocity';
     public static OUTPUT_CHANNEL_NAME = 'Vlocity';
     public static NG_APP_NAME = 'Vlocode';
