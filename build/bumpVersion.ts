620bfc18b42ad30820c86737b75c2ef2d137b085
Peter van Gulik <peter@curlybracket.nl>
2020-07-16 21:22:32 +0200

    Fixed bump script cannot read numbers

--- build/bumpVersion.ts
+++ build/bumpVersion.ts
@@ -1,15 +1,17 @@
 // Parse commands YAML into a package JSON command structure
 import * as chalk from 'chalk';
 import * as fs from 'fs-extra';
-import * as yaml from 'js-yaml';
 import * as logSymbols from 'log-symbols';
-import * as path from 'path';
 
 async function bumpVersion(type: 'major' | 'minor' | 'patch' | string) {
     const packageJson = await fs.readJSON(`package.json`);
     const packageLockJson = await fs.readJSON(`package-lock.json`);
     const currentVersion : string = packageJson.version;
-    let [ major, minor, patch ] = currentVersion.split('.').map(parseInt);
+    let [ major, minor, patch ] = currentVersion.split('.').map(parseFloat);
+
+    if ([ major, minor, patch ].some(isNaN)) {
+        throw new Error(`Failed to parse version from package JSON: ${currentVersion}`);
+    }
 
     // What to bump
     if (type == 'major') {
