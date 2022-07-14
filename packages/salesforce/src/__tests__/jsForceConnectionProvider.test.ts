d9e25986d5d839cd2dfb0bde0be94d52e9d4720a
Peter van Gulik <p.w.van.gulik@gmail.com>
2022-07-14 16:49:35 +0200

    feat: support creating connection provider from an existing jsforce connection whilst keeping all fixes and patches to jsforce active

---
+++ packages/salesforce/src/__tests__/jsForceConnectionProvider.test.ts
@@ -0,0 +1,15 @@
+import 'jest';
+
+import { container } from '@vlocode/core';
+import { JsForceConnectionProvider } from '../connection';
+
+describe('JsForceConnectionProvider', () => {
+    describe('#create', () => {
+        it('should register as JsForceConnectionProvider in container', async () => {
+            const isolatedContainer = container.new();
+            const provider = isolatedContainer.register(JsForceConnectionProvider.create({}));
+            expect(provider).toBeInstanceOf(JsForceConnectionProvider);
+            expect(isolatedContainer.get(JsForceConnectionProvider)).toBe(provider);
+        });
+    });
+});
