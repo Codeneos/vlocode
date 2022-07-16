7b0bb65bfc49f9c47be71fa58c0c1caf6a83a9d8
Peter van Gulik <p.w.van.gulik@gmail.com>
2022-07-16 18:32:48 +0200

    Update CI config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  preset: 'ts-jest',
+  collectCoverage: true,
+  coverageProvider: 'v8',
+  coverageDirectory: 'coverage',
+  coverageReporters: [ 'lcov' ],
+  reporters: ['default', 'jest-sonar'],
+  projects: [
+    "packages/util/src", 
+    "packages/core/src", 
+    "packages/salesforce/src", 
+    "packages/vlocity-deploy/src"
+  ]
+};
