b46f2e96afe006cd29159b2202a9eb677b0cfa6d
Peter van Gulik <p.w.van.gulik@gmail.com>
2022-07-18 15:12:48 +0200

    fix: improve code coverage of IoC container class. build: store coverage report as artifact

--- packages/core/src/__tests__/container.test.ts
+++ packages/core/src/__tests__/container.test.ts
@@ -1,5 +1,5 @@
 import 'jest';
-import { Logger, container, Container } from '../';
+import { Logger, container, Container, LifecyclePolicy, injectable } from '../';
 
 describe('container', () => {
 
@@ -14,6 +14,25 @@ describe('container', () => {
     class ServiceImpl extends ServiceShape {
         foo() { return 'bar'; }
         bar() { return Promise.resolve('foo'); }
+    }
+
+    @injectable()
+    class ServiceImplCircular {
+        public id = 0;
+        static instanceCounter = 0;
+        constructor(public child: CircularRef) {
+            this.id = ++ServiceImplCircular.instanceCounter;
+        }
+    }
+
+    @injectable()
+    class CircularRef {
+        constructor(public parent: ServiceImplCircular) {            
+        }
+    }
+
+    class InjectableProps {
+        @injectable.property public service: ServiceImpl;
     }
     
     beforeAll(() =>  container.registerAs(Logger.null, Logger));
@@ -50,4 +69,98 @@ describe('container', () => {
             expect(testContainer['instances'].get('BaseShape')).not.toBeUndefined();
         });
     });
+
+    describe('resolve', () => {
+        it('should prefer active instance over registered type', async () => {
+            // Arrange
+            const testContainer = new Container(Logger.null);
+            const activeInstance = new ServiceImpl();
+            testContainer.registerType(ServiceImpl, [ServiceImpl, BaseShape, ServiceShape]);
+            testContainer.register(activeInstance);
+
+            // Act + Assert
+            expect(testContainer.resolve(BaseShape)).toBe(activeInstance);
+            expect(testContainer.resolve(ServiceImpl)).toBe(activeInstance);
+            expect(testContainer.resolve(ServiceShape)).toBe(activeInstance);
+        });
+        it('should create multiple instance when lifecycle is transient', async () => {
+            // Arrange
+            const testContainer = new Container(Logger.null);
+            testContainer.registerType(ServiceImpl, [ServiceImpl, BaseShape, ServiceShape], { lifecycle: LifecyclePolicy.transient });
+
+            // Act
+            const results = [testContainer.resolve(ServiceImpl), testContainer.resolve(ServiceImpl)];
+
+            // Assert
+            expect(results[0]).not.toBe(results[1]);
+        });
+        it('should create single instance when lifecycle is singleton', async () => {
+            // Arrange
+            const testContainer = new Container(Logger.null);
+            testContainer.registerType(ServiceImpl, [ServiceImpl, BaseShape, ServiceShape], { lifecycle: LifecyclePolicy.singleton });
+
+            // Act
+            const results = [testContainer.resolve(ServiceImpl), testContainer.resolve(ServiceImpl)];
+
+            // Assert
+            expect(results[0]).toBe(results[1]);
+        });
+        it('should call factory method when a factory is registered', async () => {
+            // Arrange
+            const testContainer = new Container(Logger.null);
+            const factory = jest.fn().mockReturnValue(new ServiceImpl());
+            testContainer.registerFactory(ServiceImpl, factory);
+
+            // Act
+            testContainer.resolve(ServiceImpl);
+            testContainer.resolve(ServiceImpl);
+
+            // Assert
+            expect(factory).toBeCalledTimes(2);
+        });
+        it('should call factory method 1 once when for singleton factories', async () => {
+            // Arrange
+            const testContainer = new Container(Logger.null);
+            const factory = jest.fn().mockReturnValue(new ServiceImpl());
+            testContainer.registerFactory(ServiceImpl, factory, LifecyclePolicy.singleton);
+
+            // Act
+            testContainer.resolve(ServiceImpl);
+            testContainer.resolve(ServiceImpl);
+
+            // Assert
+            expect(factory).toBeCalledTimes(1);
+        });
+        it('should resolve circular references without creating duplicate service instances', async () => {
+            // Arrange
+            const testContainer = new Container(Logger.null);
+            testContainer.registerType(ServiceImplCircular, [ServiceImplCircular]);
+            testContainer.registerType(CircularRef, [CircularRef]);
+
+            // Act
+            const result = testContainer.resolve(ServiceImplCircular)!;
+
+            // Assert
+            expect(result).toBeInstanceOf(ServiceImplCircular);
+            expect(result.child).toBeInstanceOf(CircularRef);
+            expect(result.child.parent).not.toBeUndefined();
+            expect(result.child.parent.child).not.toBeUndefined();
+            expect(result.child.parent.id).toEqual(result.id);
+        });
+    });
+
+    describe('create', () => {
+        it('should resolve injectable properties on non-injectable class', async () => {
+            // Arrange
+            const testContainer = new Container(Logger.null);
+            testContainer.registerType(ServiceImpl, [ServiceImpl, BaseShape, ServiceShape]);
+
+            // Act
+            const instance = testContainer.create(InjectableProps);
+
+            // Assert
+            expect(instance.service).not.toBeUndefined();
+            expect(instance.service).toBeInstanceOf(ServiceImpl);
+        });
+    });    
 });
