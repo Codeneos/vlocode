2d025a17ed93e177358de91ac648302e2a4f36d7
Peter van Gulik <peter@curlybracket.nl>
2022-08-09 11:03:01 +0200

    feat: improve datapack rename and clone commands to look at matching keys This improves the way datapack renames and clone operations work and, they will now fetch the matching keys and not only ask you to change the name but also the matching keys. This makes it possible to rename even more complex datapacks such as OmniScripts and DataRaptors more easily.

--- packages/vscode-extension/src/commands/datapacks/cloneDatapackCommand.ts
+++ packages/vscode-extension/src/commands/datapacks/cloneDatapackCommand.ts
@@ -1,42 +1,65 @@
 import { VlocodeCommand } from '@root/constants';
 import { vscodeCommand } from '@root/lib/commandRouter';
+import { DatapackExportQueries } from '@root/lib/vlocity/datapackExportQueries';
+import { container } from '@vlocode/core';
+import { VlocityDatapack } from '@vlocode/vlocity-deploy';
+import path = require('path');
 import * as vscode from 'vscode';
 import { DatapackCommand } from './datapackCommand';
 
 @vscodeCommand(VlocodeCommand.cloneDatapack)
 export default class CloneDatapackCommand extends DatapackCommand {
 
-    constructor() {
-        super();
+    protected get exportQueries(): DatapackExportQueries {
+        return container.get(DatapackExportQueries);
     }
 
     public execute(...args: any[]) : Promise<void> {
-        return this.cloneDatapack(args[1] || [args[0] || this.currentOpenDocument]);
+        return this.cloneDatapacks(args[1] || [args[0] || this.currentOpenDocument]);
     }
 
-    protected async cloneDatapack(selectedFiles: vscode.Uri[]) : Promise<any> {
+    protected async cloneDatapacks(selectedFiles: vscode.Uri[]) : Promise<any> {        
         const datapacks = await this.vlocode.withProgress('Loading datapack...', () => this.loadDatapacks(selectedFiles));
-
         for (const datapack of datapacks) {
-            if (!datapack || datapack.name === undefined) {
-                this.logger.warn(`Datapack ${datapack.name} cannot be cloned as it doesn't have a name property`);
-                void vscode.window.showWarningMessage('The datapack does not have a Name property');
-                continue;
-            }
-
-            const newName = await vscode.window.showInputBox({
-                prompt: `Name for clone of ${datapack.name}...`,
-                ignoreFocusOut: true,
-                value: `${datapack.name}-clone`,
-            });
-            if (!newName) {
-                return;
-            }
-
-            datapack.rename(newName);
-            datapack.regenerateGlobalKey();
-
-            await this.vlocode.withProgress(`Cloning ${datapack.name}...`, () => this.vlocode.datapackService.expandDatapack(datapack, datapack.projectFolder));
+            await this.cloneDatapack(datapack, { updateGlobalKey: true });  
         }
+    }
+
+    protected async cloneDatapack(datapack: VlocityDatapack, options?: { updateGlobalKey?: boolean }) : Promise<any> {
+        const matchingFields = await this.exportQueries.getMatchingFields(datapack.datapackType);
+
+        if (!matchingFields) {
+            return vscode.window.showWarningMessage('The selected datapack does not have any matching fields');
+        }
+
+        if (datapack.name && !matchingFields.includes('Name')) {
+            matchingFields.push('Name');
+        }
+
+        for (const field of matchingFields) {
+            const value = await vscode.window.showInputBox({
+                prompt: `Enter ${field}...`,
+                ignoreFocusOut: true,
+                value: datapack[field]
+            });  
+            
+            if (value === undefined) {
+                return;
+            }            
+  
+            if (field === 'Name') {
+                datapack.rename(value);
+            } else {
+                datapack.updateField(field, value);
+            }
+        }
+
+        // Regen global keys
+        if (options?.updateGlobalKey) {
+            datapack.regenerateGlobalKey();
+        }
+
+        // execute rename and expand into new folder structure
+        await this.vlocode.withProgress(`Expanding ${datapack.name}...`, () => this.vlocode.datapackService.expandDatapack(datapack, datapack.projectFolder));
     }
 }
