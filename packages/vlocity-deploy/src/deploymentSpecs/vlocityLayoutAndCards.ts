b565f6c67df47c5d6807b8392d691afabc578dce
Peter van Gulik <peter@curlybracket.nl>
2022-10-28 21:36:49 +0200

    feat: combine layout and card deployment actions

---
+++ packages/vlocity-deploy/src/deploymentSpecs/vlocityLayoutAndCards.ts
@@ -0,0 +1,10 @@
+import type { DatapackDeploymentSpec } from '../datapackDeploymentSpec';
+import { VlocityDatapack } from '../datapack';
+import { deploymentSpec } from '../datapackDeploymentSpecRegistry';
+
+@deploymentSpec({ datapackFilter: /^Vlocity(UILayout|Card)$/i })
+export class VlocityUILayoutAndCards implements DatapackDeploymentSpec {
+    public async preprocess(datapack: VlocityDatapack) {
+        datapack.Active__c = true;
+    }
+}
