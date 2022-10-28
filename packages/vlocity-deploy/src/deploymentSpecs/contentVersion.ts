f7bc2b81ea9f901e41480eded09a563fbfa47323
Peter van Gulik <peter@curlybracket.nl>
2022-10-28 21:37:44 +0200

    fix: content versions create new content documents during deployment instead of attaching to the existing content document as new version

--- packages/vlocity-deploy/src/deploymentSpecs/contentVersion.ts
+++ packages/vlocity-deploy/src/deploymentSpecs/contentVersion.ts
@@ -1,12 +1,49 @@
-import { injectable, LifecyclePolicy } from '@vlocode/core';
-import { DatapackDeploymentRecord } from '../datapackDeploymentRecord';
-import type { DatapackDeploymentSpec } from '../datapackDeploymentSpec';
+import { DeferredWorkQueue, Logger, WorkItemResult } from '@vlocode/core';
+import { SalesforceLookupService } from '@vlocode/salesforce';
+import { deploymentSpec } from '../datapackDeploymentSpecRegistry';
+import { DatapackDeploymentRecord, DeploymentAction } from '../datapackDeploymentRecord';
+import { DatapackDeploymentSpec } from '../datapackDeploymentSpec';
 
-@injectable({ lifecycle: LifecyclePolicy.transient })
+interface ContentVersionRecord {
+    ContentDocumentId: string,
+    VersionNumber: number
+}
+
+@deploymentSpec({ recordFilter: /^ContentVersion$/i })
 export class ContentVersion implements DatapackDeploymentSpec {
-    public async afterRecordConversion(records: ReadonlyArray<DatapackDeploymentRecord>) {
-        for (const record of records) {
-            record.skipLookup = true;
+
+    private lookupQueue = new DeferredWorkQueue(this.lookupProcessor, this);
+
+    public constructor(
+        private readonly lookupService: SalesforceLookupService,
+        private readonly logger: Logger) {
+    }
+
+    public beforeDeployRecord(records: ReadonlyArray<DatapackDeploymentRecord>) {
+        return Promise.all(records.map(this.updateContentDocumentId, this));
+    }
+
+    private async lookupProcessor(ids: string[]) {
+        const lookupFilters = ids.map(id => ({ id }));
+        const results = await this.lookupService.lookup('ContentVersion', lookupFilters, ['id', 'ContentDocumentId', 'VersionNumber']);
+
+        return ids.map<WorkItemResult<ContentVersionRecord>>(id => ({
+            status: 'fulfilled',
+            value: results.find(record => record.id == id)!
+        }));
+    }
+
+    private async updateContentDocumentId(record: DatapackDeploymentRecord) {
+        if (!record.recordId) {
+            return;
         }
+
+        const currentContentVersion = await (this.lookupQueue.getQueuedWork(item => item === record.recordId) ?? this.lookupQueue.enqueue(record.recordId))    ;    
+        if (!currentContentVersion) {
+            return;
+        }
+
+        record.setAction(DeploymentAction.Insert);
+        record.value('ContentDocumentId', currentContentVersion.ContentDocumentId);
     }
 }
