b10e7f0c428efe8ab2f9d5cd62834b4914e7667a
Peter van Gulik <peter@curlybracket.nl>
2022-10-28 21:36:30 +0200

    fix: override definitions are not correctly updated causing them to break

---
+++ packages/vlocity-deploy/src/datapackDeploymentSpecRegistry.ts
@@ -0,0 +1,98 @@
+import { Container, container, injectable, LifecyclePolicy, Logger, ServiceCtor, ServiceType } from "@vlocode/core";
+import { lazy } from "@vlocode/util";
+import { randomUUID } from "crypto";
+import { DatapackFilter } from "datapackDeployer";
+import { DatapackDeploymentSpec } from "datapackDeploymentSpec";
+
+@injectable({ lifecycle: LifecyclePolicy.transient })
+export class DatapackDeploymentSpecRegistry {
+
+    /**
+     * Get the current global {@link DatapackDeploymentSpecRegistry} instance registered in the root container
+     */
+    public static readonly instance = lazy(() => container.get(DatapackDeploymentSpecRegistry));
+    public static specCounter = 0;
+
+    @injectable.property 
+    private readonly logger: Logger;
+    private readonly specs: Record<number, { 
+        key: number,
+        filter: DatapackFilter, 
+        type?: ServiceCtor<DatapackDeploymentSpec>, 
+        instance?: DatapackDeploymentSpec 
+    }> = {};
+
+    constructor(private readonly container: Container) {
+    }
+
+    /**
+     * Standard iterator to iterate over the specs in this registry
+     * @returns Iterable
+     */
+    public *getSpecs() {
+        for (const globalSpec of Object.values(DatapackDeploymentSpecRegistry.instance.specs)) {
+            if (this.specs[globalSpec.key]) {
+                continue;
+            }
+
+            // Copy global specs to local instance; do not copy instances when there is a spec-type set
+            this.specs[globalSpec.key] = { 
+                key: globalSpec.key,
+                filter: globalSpec.filter,
+                type: globalSpec.type,
+                instance: !globalSpec.type ? globalSpec.instance : undefined,
+            };
+        }
+
+        for (const spec of Object.values(this.specs)) {
+            yield { 
+                filter: spec.filter, 
+                spec: spec.instance ?? lazy( () => this.container.create(spec.type!) )
+            };
+        }
+    }
+
+    /**
+     * Register an individual spec function to be executed in for the datapacks of the specified type in this deployment.
+     * @param datapackType type of the Datapack 
+     * @param fn name of the hook function
+     * @param executor function executed
+     */
+    public registerFunction<T extends keyof DatapackDeploymentSpec>(datapackType: string, fn: T, executor: DatapackDeploymentSpec[T]) {
+        this.register(datapackType, { [fn]: executor });
+    }
+
+    /**
+     * Register a spec with 1 or more hooks to be executed in for the datapacks of the specified type in this deployment. 
+     * @param filter filter that determines when this datapack is applied
+     * @param spec Object matching the {@link DatapackDeploymentSpec}-shape
+     */
+    public register(filter: DatapackFilter | string, spec: DatapackDeploymentSpec | ServiceCtor<DatapackDeploymentSpec>) {
+        if (typeof filter === 'string') {
+            filter = { datapackFilter: filter };
+        }
+
+        const filterName = filter.datapackFilter ? `datapacks: '${filter.datapackFilter.toString()}'` : `records: '${filter.recordFilter?.toString()}'`;
+        this.logger.verbose(`Register datapack deployment spec for ${filterName}`)
+        
+        this.specs[++DatapackDeploymentSpecRegistry.specCounter] = {
+            key: DatapackDeploymentSpecRegistry.specCounter, 
+            filter, 
+            type: typeof spec === 'function' ? spec : undefined,
+            instance: typeof spec !== 'function' ? spec : undefined,
+        };
+    }
+}
+
+/**
+ * Classes decorated with this decorator will be registered in the {@link DatapackDeploymentSpecRegistry} and in the global-container as transient service types.
+ * @param filter filter that determines to which datapacks or sobject-records this deployment spec applies
+ */
+export function deploymentSpec(filter: DatapackFilter) {
+    const containerDecorator = injectable({ lifecycle: LifecyclePolicy.transient });
+    return function (constructor: any) {
+        constructor = !injectable.isDecorated(constructor) ? containerDecorator(constructor) : constructor;
+        DatapackDeploymentSpecRegistry.instance.register(filter, constructor);
+        return constructor;
+    };
+}
---
+++ packages/vlocity-deploy/src/deploymentSpecs/overrideDefinitionRecords.ts
@@ -0,0 +1,46 @@
+import { DeferredWorkQueue, Logger, WorkItemResult } from '@vlocode/core';
+import { SalesforceLookupService } from '@vlocode/salesforce';
+import { deploymentSpec } from '../datapackDeploymentSpecRegistry';
+import { DatapackDeploymentRecord } from '../datapackDeploymentRecord';
+import { DatapackDeploymentSpec } from '../datapackDeploymentSpec';
+
+@deploymentSpec({ recordFilter: /OverrideDefinition__c$/i })
+export class OverrideDefinitionRecords implements DatapackDeploymentSpec {
+
+    private lookupQueue = new DeferredWorkQueue(this.lookupProcessor, this);
+
+    public constructor(
+        private readonly lookupService: SalesforceLookupService,
+        private readonly logger: Logger) {
+    }
+
+    public beforeDeployRecord(records: ReadonlyArray<DatapackDeploymentRecord>) {
+        return Promise.all(records.map(this.updateOverrideDefinition, this));
+    }
+
+    private async lookupProcessor(globalKeys: string[]) {
+        const lookupFilters = globalKeys.map(key => ({ 'GlobalKey__c': key }));
+        const results = await this.lookupService.lookup('Product2', lookupFilters, ['id', 'GlobalKey__c']);
+
+        return globalKeys.map<WorkItemResult<string | undefined>>(key => ({
+            status: 'fulfilled',
+            value: results.find(record => record.GlobalKey__c == key)?.Id
+        }));
+    }
+
+    private async updateOverrideDefinition(record: DatapackDeploymentRecord) {
+        const globalKeyPath = record.value('ProductHierarchyGlobalKeyPath__c');
+        
+        if (typeof globalKeyPath !== 'string') {
+            return;
+        }
+
+        const resolvedProductIds = await Promise.all(
+            globalKeyPath.split('<').map(key => this.lookupQueue.getQueuedWork(item => item === key) ?? this.lookupQueue.enqueue(key))
+        );
+
+        const resolvedPath = resolvedProductIds.join('<');
+        this.logger.verbose(`Update override hierarchy path: ${resolvedPath}`);
+        record.value('ProductHierarchyPath__c', resolvedPath);
+    }
+}
