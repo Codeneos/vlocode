0c7e23cc73c9396452c05827d361c29585b79b0f
Peter van Gulik <peter@curlybracket.nl>
2022-10-31 14:14:27 +0100

    feat: automatically create ContentDocumentLinks post deployment to make document templates available post-deployment without manual actions

--- packages/vlocity-deploy/src/deploymentSpecs/contentVersion.ts
+++ packages/vlocity-deploy/src/deploymentSpecs/contentVersion.ts
@@ -1,9 +1,10 @@
 import { DeferredWorkQueue, Logger, WorkItemResult } from '@vlocode/core';
-import { SalesforceLookupService } from '@vlocode/salesforce';
+import { SalesforceLookupService, SalesforceService } from '@vlocode/salesforce';
 import { deploymentSpec } from '../datapackDeploymentSpecRegistry';
 import { DatapackDeploymentRecord, DeploymentAction } from '../datapackDeploymentRecord';
 import { DatapackDeploymentSpec } from '../datapackDeploymentSpec';
 import { createHash } from 'crypto';
+import { DatapackDeploymentEvent } from 'datapackDeployer';
 
 interface ContentVersionRecord {
     Checksum: string;
@@ -11,18 +12,23 @@ interface ContentVersionRecord {
     VersionNumber: number;
 }
 
-@deploymentSpec({ recordFilter: /^ContentVersion$/i })
+@deploymentSpec({ datapackFilter: /^ContentVersion$/i })
 export class ContentVersion implements DatapackDeploymentSpec {
 
     private lookupQueue = new DeferredWorkQueue(this.lookupProcessor, this);
 
     public constructor(
+        private readonly salesforceService: SalesforceService,
         private readonly lookupService: SalesforceLookupService,
         private readonly logger: Logger) {
     }
 
+    public afterDeploy(event: DatapackDeploymentEvent) {        
+        return this.createContentDocumentLinks(event);
+    }
+
     public beforeDeployRecord(records: ReadonlyArray<DatapackDeploymentRecord>) {
-        return Promise.all(records.map(this.updateContentDocumentId, this));
+        return Promise.all(records.map(rec => this.updateContentDocumentId(rec)));
     }
 
     private async lookupProcessor(ids: string[]) {
@@ -35,12 +41,38 @@ export class ContentVersion implements DatapackDeploymentSpec {
         }));
     }
 
-    private async updateContentDocumentId(record: DatapackDeploymentRecord) {
-        if (!record.recordId) {
-            return;
-        }
+    /**
+     * Create `ContentDocumentLink` object for the just deployed ContentVersion's objects making
+     * them available for the whole organization
+     * @param event 
+     */
+    private async createContentDocumentLinks(event: DatapackDeploymentEvent) {
+        for (const record of event.getDeployedRecords('ContentVersion')) {
+            const documentLinkValues = await this.getContentDocumentLinkValues(record);
 
-        const currentContentVersion = await (this.lookupQueue.getQueuedWork(item => item === record.recordId) ?? this.lookupQueue.enqueue(record.recordId))    ;    
+            if (!documentLinkValues) {
+                continue;
+            }
+
+            // Add ContentDocumentLink creation into deployment
+            event.deployment.add(DatapackDeploymentRecord.fromValues(
+                'ContentDocumentLink', 
+                documentLinkValues,
+                { 
+                    upsertFields: [ 'ContentDocumentId', 'LinkedEntityId' ],                 
+                    recordKey: `ContentDocumentLink/${record.datapackKey}`
+                }
+            ));
+        }
+    }
+
+    /**
+     * Link the ContentVersion objects to existing content document objects. If the content version is not changed
+     * skip deployment of a mew ContentVersion to save space on the target org. 
+     * @param record ContentVersion record
+     */
+    private async updateContentDocumentId(record: DatapackDeploymentRecord) {
+        const currentContentVersion = await this.lookupContentVersion(record);
         if (!currentContentVersion) {
             return;
         }
@@ -57,11 +89,42 @@ export class ContentVersion implements DatapackDeploymentSpec {
         }
     }
 
+    private async lookupContentVersion(record: DatapackDeploymentRecord) {
+        if (!record.recordId) {
+            return;
+        }
+        return (this.lookupQueue.getQueuedWork(item => item === record.recordId) ?? await this.lookupQueue.enqueue(record.recordId));
+    }
+
+    /**
+     * Calculate the MD5 checksum of the VersionData of a ContentVersion record using the same logic as used
+     * by Salesforce to calculate the value in the `ContentVersion.Checksum` field.
+     * @param record ContentVersion deployment record
+     * @returns MD5 checksum
+     */
     private calculateVersionDataChecksum(record: DatapackDeploymentRecord) {
         try {
             return createHash('md5').update(Buffer.from(record.value('VersionData'), 'base64')).digest("hex");
         } catch(err) {
             record.addWarning(`Unable to digest md5 checksum due to crypto error: ${err.message}`);
         }
+    }
+
+    private async getContentDocumentLinkValues(record: DatapackDeploymentRecord) {
+        if (!record.isSkipped) {
+            return;
+        }
+
+        const contentVersion = await this.lookupContentVersion(record);
+        if (!contentVersion) {
+            return;
+        }
+
+        return {
+            ContentDocumentId: contentVersion.ContentDocumentId,
+            ShareType: 'V',
+            Visibility: 'AllUsers',
+            LinkedEntityId: (await this.salesforceService.getOrganizationDetails()).id
+        };
     }
 }
