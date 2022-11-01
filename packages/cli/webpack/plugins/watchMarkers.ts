e43e24a6d3fccff5fc5d7d94b4aded25a8a34d1b
Peter van Gulik <peter@curlybracket.nl>
2022-11-01 10:22:22 +0100

    build: fix watch markers printed in debug lines for CLI build

--- packages/cli/webpack/plugins/watchMarkers.ts
+++ packages/cli/webpack/plugins/watchMarkers.ts
@@ -1,12 +1,9 @@
 import { WebpackPluginInstance, Compiler } from 'webpack';
 
-console.info('WatchMarkersPlugin');
 export default class WatchMarkersPlugin implements WebpackPluginInstance {
     private buildCounter = 0;
 
     public apply(compiler: Compiler) {
-        
-        console.info('WatchMarkersPlugin->apply');
         compiler.hooks.infrastructureLog.tap('WatchMarkersPlugin', (arg0, arg1, arg2) => {
             const messages = arg2.filter(m => typeof m === 'string');
             const isCompileStart = messages.some(msg => /Compiler '[\w\d\-_]+' starting... /i.test(msg));
