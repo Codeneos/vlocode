77f8566d6b4edddb9dde6f08811a66ed04f53089
Peter van Gulik <peter@curlybracket.nl>
2022-11-02 18:43:13 +0100

    build: remove "recommended-requiring-type-checking" as it doesn't work reliably

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,8 +18,7 @@ module.exports = {
     ],
     extends: [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+        "plugin:@typescript-eslint/recommended"
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
