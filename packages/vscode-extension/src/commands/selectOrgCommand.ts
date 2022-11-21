3487470be054f32eb628970b02d1bdb557e3020a
Peter van Gulik <peter@curlybracket.nl>
2022-11-21 18:21:50 +0100

    chore: code quality issue reported by sonarqube

--- packages/vscode-extension/src/commands/selectOrgCommand.ts
+++ packages/vscode-extension/src/commands/selectOrgCommand.ts
@@ -3,7 +3,6 @@ import { SalesforceOrgInfo, sfdx } from '@vlocode/util';
 import { CommandBase } from '../lib/commandBase';
 import { vscodeCommand } from '@root/lib/commandRouter';
 import { VlocodeCommand } from '@root/constants';
-import chalk = require('chalk');
 
 type SelectOrgQuickPickItem = vscode.QuickPickItem & { orgInfo?: SalesforceOrgInfo; instanceUrl?: string };
 
@@ -65,7 +64,6 @@ export default class SelectOrgCommand extends CommandBase {
             await this.vlocode.refreshOAuthTokens();
         } else {
             const selectedOrgInfo = selectedOrg.orgInfo || await this.authorizeNewOrg();
-            chalk
             if (selectedOrgInfo) {
                 this.logger.log(`Connecting to: ${selectedOrgInfo.username}...`);
                 if (this.vlocode.config.sfdxUsername != selectedOrgInfo.username) {
