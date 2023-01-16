77584386c74375fa870bfa83a4f2acb43937a04c
Peter van Gulik <peter@curlybracket.nl>
2023-01-16 12:12:42 +0100

    feat: ensure correct deployment order for Vlocity cards

--- packages/util/src/guards.ts
+++ packages/util/src/guards.ts
@@ -10,3 +10,17 @@ export function isUndefinedOrNull(arg: any): arg is (undefined | null) {
 export function isReadonlyArray<T>(arg: unknown): arg is readonly T[] {
     return Array.isArray(arg) || arg instanceof Array;
 }
+
+
+/**
+ * Type-check helper method that checks the specified parameter for null or undefined and 
+ * throws an exception when the value is null or undefined or returns the specified value if it is not null.
+ * @param value Value ot check
+ * @param error Optional error message to throw when the value is null 
+ */
+export function nullCheck<T>(value: T, error = 'Value cannot be undefined or null'): NonNullable<T> {
+    if (value === null && value === undefined) { 
+        throw new Error(error);
+    }
+    return value as NonNullable<T>;
+}
--- packages/vlocity-deploy/src/deploymentSpecs/vlocityLayoutAndCards.ts
+++ packages/vlocity-deploy/src/deploymentSpecs/vlocityLayoutAndCards.ts
@@ -3,6 +3,7 @@ import { VlocityDatapack } from '../datapack';
 import { deploymentSpec } from '../datapackDeploymentSpecRegistry';
 import { RecordActivator } from './recordActivator';
 import { DatapackDeploymentEvent } from '../datapackDeploymentEvent';
+import { DatapackDeploymentRecord } from '../datapackDeploymentRecord';
 
 @deploymentSpec({ recordFilter: /^Vlocity(UILayout|Card)__c$/i })
 export class VlocityUILayoutAndCards implements DatapackDeploymentSpec {
@@ -18,6 +19,51 @@ export class VlocityUILayoutAndCards implements DatapackDeploymentSpec {
     public async preprocess(datapack: VlocityDatapack) {
         // Update to inactive to allow insert; later in the process these are activated
         datapack.data['%vlocity_namespace%__Active__c'] = false;
+    }
+
+    public afterRecordConversion(records: ReadonlyArray<DatapackDeploymentRecord>) {
+        for (const record of records) {
+            if (record.isSObjectOfType('VlocityCard__c')) {
+                this.addCardStateDependencies(record);
+            }            
+        }
+    }
+
+    /**
+     * Adds card dependencies based on the child cards mentioned in the card states object. Ensures that child cards are deployed
+     * and activated before their parent is deployed and activated
+     * @param record Datapack deployment record to validate
+     */
+    public addCardStateDependencies(record: DatapackDeploymentRecord) {
+        let cardDefinition = record.value('Definition__c');
+        if (typeof cardDefinition === 'string') {
+            try {
+                cardDefinition = JSON.parse(cardDefinition);
+            } catch(err) {
+                record.addWarning('Unable to parse card definition as JSON');
+                return;
+            }            
+        }
+
+        if (typeof cardDefinition !== 'object' && !Array.isArray(cardDefinition.states)) {
+            return;
+        }
+
+        for (const cardState of cardDefinition.states) {
+            if (!Array.isArray(cardState.childCards) && !cardState.childCards.length) {
+                continue;
+            }
+
+            for (const childCardName of cardState.childCards) {
+                record.addDependency({
+                    Name: childCardName,
+                    VlocityRecordSObjectType: '%vlocity_namespace%__VlocityCard__c', 
+                    VlocityDataPackType: 'VlocityLookupMatchingKeyObject',
+                    VlocityLookupRecordSourceKey: `%vlocity_namespace%__VlocityCard__c/${childCardName}`,
+                    VlocityMatchingRecordSourceKey: undefined
+                });
+            }
+        }
     }
 
     public afterDeploy(event: DatapackDeploymentEvent) {
