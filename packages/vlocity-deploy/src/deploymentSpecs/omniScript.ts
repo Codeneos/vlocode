c9c08f1774fec7cda1c157fbf03c8e9e5fba8768
Peter van Gulik <peter@curlybracket.nl>
2023-03-10 16:26:30 +0100

    fix: update order and level of OmniScript elements before deployment (issue #396)

--- packages/vlocity-deploy/src/deploymentSpecs/omniScript.ts
+++ packages/vlocity-deploy/src/deploymentSpecs/omniScript.ts
@@ -1,12 +1,22 @@
 import { Container, Logger } from '@vlocode/core';
 import { SalesforceDeployService, SalesforcePackage } from '@vlocode/salesforce';
-import { forEachAsyncParallel, getErrorMessage, Iterable, Timer } from '@vlocode/util';
+import { forEachAsyncParallel, getErrorMessage, Iterable, mapGetOrCreate, substringAfter, Timer } from '@vlocode/util';
 import { DatapackDeploymentRecord, DeploymentStatus } from '../datapackDeploymentRecord';
 import { VlocityDatapack } from '../datapack';
 import { DatapackDeploymentEvent } from '../datapackDeploymentEvent';
 import type { DatapackDeploymentSpec } from '../datapackDeploymentSpec';
 import { OmniScriptActivator } from '../omniScript/omniScriptActivator';
 import { deploymentSpec } from '../datapackDeploymentSpecRegistry';
+
+interface ScriptElement {
+    VlocityRecordSourceKey: string;
+    Name: string;
+    ParentElementId__c?: {
+        VlocityMatchingRecordSourceKey: string;
+    };
+    Level__c?: number;
+    Order__c?: number;
+}
 
 @deploymentSpec({ 
     datapackFilter: /^(OmniScript|IntegrationProcedure)$/i, 
@@ -16,6 +26,17 @@ export class OmniScript implements DatapackDeploymentSpec {
 
     private readonly lwcEnabledDatapacks = new Set<string>();
     private readonly embeddedTemplates = new Map<string, string[]>();
+    private readonly preprocessorMessages = new Map<string, string[]>();
+
+    /**
+     * Number of parallel activations to perform.
+     */
+    public static ParallelActivations = 4;
+
+    /**
+     * Number of parallel tooling deployments to perform.
+     */
+    public static ParallelToolingDeployments = 4;
 
     public constructor(
         private readonly activator: OmniScriptActivator,
@@ -34,9 +55,59 @@ export class OmniScript implements DatapackDeploymentSpec {
             this.embeddedTemplates.set(datapack.key, [...Iterable.map(results, match => match[1])]);
         }
 
+        // Update the order of the elements in the datapack before deployment
+        this.updateElementOrder(datapack);
+
         // Insert as inactive and update later in the process these are activated
         datapack.IsActive__c = false;
         delete datapack.Version__c;
+    }
+
+    /**
+     * Update the order and level of the elements in the OmniScript or IntegrationProcedure.
+     * @param datapack 
+     */
+    private async updateElementOrder(datapack: VlocityDatapack) {
+        // Map elements by their source key so we can easily lookup the parent elements
+        const elementsByKey = new Map<string, ScriptElement>(datapack.Element__c.map(element => [element.VlocityRecordSourceKey, element]) );
+        
+        const getElementLevel = (element: ScriptElement) => {
+            const parentElementPath = new Array<ScriptElement>();
+            while(element) {
+                const parentKey = element.ParentElementId__c?.VlocityMatchingRecordSourceKey;
+                const parent = parentKey && elementsByKey.get(parentKey);
+                if (!parent) {
+                    break;                
+                }
+                parentElementPath.push(element = parent);
+            }
+            return parentElementPath.length;
+        };
+
+        // Iterate over the elements and update the order and level
+        // based on the order of the elements in the elements array in the datapack
+        const elementCountByParent = new Map<string, number>();
+        for (const element of datapack.Element__c as ScriptElement[]) {
+            const parentKey = element.ParentElementId__c?.VlocityMatchingRecordSourceKey ?? 'root';
+            const orderInParent = elementCountByParent.get(parentKey) ?? 0;
+
+            const currentLevel = element['%vlocity_namespace%__Level__c'];
+            const currentOrder = element['%vlocity_namespace%__Order__c'];
+            const calculatedLevel = getElementLevel(element);
+
+            if (currentOrder !== undefined && currentOrder !== orderInParent) {
+                this.addPreprocessingWarning(datapack, `element "${element.Name}" order changed: ${currentOrder} -> ${orderInParent}`);
+            }
+
+            if (currentLevel !== undefined && currentLevel !== calculatedLevel) {
+                this.addPreprocessingWarning(datapack, `element "${element.Name}" level changed: ${currentLevel} -> ${calculatedLevel}`);
+            }
+
+            element['%vlocity_namespace%__Order__c'] = orderInParent;
+            element['%vlocity_namespace%__Level__c'] = calculatedLevel;
+
+            elementCountByParent.set(parentKey, orderInParent + 1);
+        }
     }
 
     public afterRecordConversion(records: ReadonlyArray<DatapackDeploymentRecord>) {
@@ -48,6 +119,7 @@ export class OmniScript implements DatapackDeploymentSpec {
                 this.addElementDependencies(record);
             }
         }
+        this.addPreprocessorMessages(records);
     }
 
     private addElementDependencies(record: DatapackDeploymentRecord) {
@@ -82,7 +154,7 @@ export class OmniScript implements DatapackDeploymentSpec {
     }
 
     /**
-     * Activate the Omniscripts in Parallel using the activator without deploying the LWC components
+     * Activate the OmniScripts in Parallel using the activator without deploying the LWC components
      * @param event 
      */
     private async activateScripts(event: DatapackDeploymentEvent) {
@@ -92,10 +164,10 @@ export class OmniScript implements DatapackDeploymentSpec {
                 await this.activator.activate(record.recordId, { skipLwcDeployment: true });
                 this.logger.info(`Activated ${record.datapackKey} [${timer.stop()}]`);
             } catch(err) {
-                this.logger.error(`Failed to activate ${record.datapackKey} -- ${err}`);
-                record.updateStatus(DeploymentStatus.Failed, err.message || err);
+                this.logger.error(`Failed to activate ${record.datapackKey} -- ${getErrorMessage(err)}`);
+                record.updateStatus(DeploymentStatus.Failed, getErrorMessage(err));
             }
-        }, 4);
+        }, OmniScript.ParallelActivations);
     }
 
     /**
@@ -113,10 +185,10 @@ export class OmniScript implements DatapackDeploymentSpec {
                     await this.activator.activateLwc(record.recordId, { toolingApi: true });
                 }
             } catch(err) {
-                this.logger.error(`Failed to deploy LWC component for ${record.datapackKey} -- ${getErrorMessage(err, true)}`);
+                this.logger.error(`Failed to deploy LWC component for ${record.datapackKey} -- ${getErrorMessage(err)}`);
                 record.updateStatus(DeploymentStatus.Failed, err.message || err);
             }
-        }, 8);
+        }, OmniScript.ParallelToolingDeployments);
 
         if (packages.length) {
             const timer = new Timer();
@@ -124,5 +196,32 @@ export class OmniScript implements DatapackDeploymentSpec {
             await this.container.create(SalesforceDeployService, undefined, Logger.null).deployPackage(packages.reduce((p, c) => p.merge(c)));
             this.logger.info(`Deployed ${packages.length} LWC components [${timer.stop()}]`);
         }
+    }
+
+    /**
+     * Add a warning message to a record from the preprocessing stage; stores the messages and adds them to the records in the afterRecordConversion stage.
+     * 
+     * All pre-processor messages are warnings, if the pre-processor encounters an error it should throw an exception.
+     * 
+     * @param sourceKey Source key of the future record to add the warning to
+     * @param message Message to add
+     */
+    private addPreprocessingWarning(sourceKey: string | { VlocityRecordSourceKey?: string, sourceKey?: string }, message: string) {
+        sourceKey = typeof sourceKey === 'string' ? sourceKey : sourceKey.VlocityRecordSourceKey ?? sourceKey.sourceKey!;        
+        this.logger.warn(`${substringAfter(sourceKey.replaceAll('%vlocity_namespace%__', ''), '/')} -- ${message}`);
+        mapGetOrCreate(this.preprocessorMessages, sourceKey, () => new Array<string>()).push(message);
+    }
+
+    /**
+     * Should be called in the afterRecordConversion stage to add the preprocessor messages to the records
+     * @param records Records to add the preprocessor messages to
+     */
+    private addPreprocessorMessages(records: ReadonlyArray<DatapackDeploymentRecord>) {
+        for (const record of records) {
+            for (const message of this.preprocessorMessages.get(record.sourceKey) ?? []) {
+                record.addWarning(message);
+            }
+        }
+        this.preprocessorMessages.clear();
     }
 }
