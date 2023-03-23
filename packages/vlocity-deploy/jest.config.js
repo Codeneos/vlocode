65fb3a975cc291fbb37b4dc04eea9afe97605924
Peter van Gulik <peter@curlybracket.nl>
2023-03-23 15:37:06 +0100

    fix: only run tests for test and spec files in __tests__ folder

--- packages/vlocity-deploy/jest.config.js
+++ packages/vlocity-deploy/jest.config.js
@@ -3,6 +3,10 @@ module.exports = {
   roots: [ 'src' ],
   collectCoverage: true,
   coveragePathIgnorePatterns: [ '/node_modules/', 'index.ts' ],
+  testRegex: [
+    "/__tests__/\\.test\\.[jt]sx?$",
+    "/__tests__/\\.spec\\.[jt]sx?$"
+  ],
   globals: {
     'ts-jest': {
       isolatedModules: true,
