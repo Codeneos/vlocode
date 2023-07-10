2fb22423ba4c7986ad2a9a6ec4f481cb0799ac06
Peter van Gulik <peter@curlybracket.nl>
2023-07-10 20:53:40 +0200

    feat: add retryable decorator to retry async operations x times before failing

--- packages/util/src/async.ts
+++ packages/util/src/async.ts
@@ -46,7 +46,7 @@ export function isThenable(value: any): value is PromiseLike<any> {
  * @returns Promise
  */
 export async function poll<T>(callback: () => OptionalPromise<T | undefined>, timeout: number, interval: number = 50, options?: { rejectionMessage?: string, resolveOnTimeout?: boolean }): Promise<T> {
-    const timeoutTime = new Date().getTime() + timeout;    
+    const timeoutTime = new Date().getTime() + timeout;
     return new Promise<T>((resolve, reject) => {
         const pollFn = async () => {
             const value = await callback();
@@ -67,36 +67,36 @@ export async function poll<T>(callback: () => OptionalPromise<T | undefined>, ti
 }
 
 export function resumeOnce<A, E extends string | symbol = string>(
-    eventName: E, 
-    emitter: { 
-        once(eventName: E, listener: (arg: A) => any) 
+    eventName: E,
+    emitter: {
+        once(eventName: E, listener: (arg: A) => any)
     },
     timeout?: number
 ): Promise<A>;
 
 export function resumeOnce<A extends any[], E extends string | symbol = string>(
-    eventName: E, 
-    emitter: { 
-        once(eventName: E, listener: (...arg: A) => any) 
+    eventName: E,
+    emitter: {
+        once(eventName: E, listener: (...arg: A) => any)
     },
     timeout?: number
 ): Promise<A>;
 
 /**
- * Await an event from an emitter and resume once the event is emitted. 
- * If the event results any results they are returned as result of the promise. 
+ * Await an event from an emitter and resume once the event is emitted.
+ * If the event results any results they are returned as result of the promise.
  * When the event returns a single result it is returned as single item, when the event emits
  * @param eventName Name of the event to listen to
  * @param emitter Emitter emitting the event
  * @param timeout Optional timeout in ms, if no event is emitted within the timeout an exception os thrown. Defaults to indefinitely
  */
 export function resumeOnce<A, E extends string | symbol>(
-    eventName: E, 
-    emitter: { 
-        once(eventName: E, listener: (...args: A[]) => any) 
+    eventName: E,
+    emitter: {
+        once(eventName: E, listener: (...args: A[]) => any)
     },
     timeout?: number
-): Promise<A> {    
+): Promise<A> {
     return new Promise<A>((resolve, reject) => {
         let isTimeout = false;
         timeout && wait(timeout).then(() => {
@@ -121,14 +121,91 @@ export function thenCall<P extends PromiseLike<T>, T = Awaited<P>>(promise: P, c
     if (!callback) {
         return promise;
     }
-    
+
     if (isThenable(promise)) {
         return promise.then((value) => {
             callback(undefined, value);
             return value;
         }, (err) => callback(err, undefined));
     }
-    
-    callback(undefined, promise);    
+
+    callback(undefined, promise);
     return Promise.resolve(promise);
+}
+
+interface RetryableOptions<T = any> { 
+    maxRetries?: number,
+    interval?: number,
+    rejectionMessage?: string,
+    resolveOnTimeout?: T,
+}
+
+/**
+ * Make any Async function that returns a promise retryable by with a configurable number of retries and interval between retries.
+ *
+ * By default the function will retry 3 times with an interval of 500ms between retries. When the function still fails after the
+ * configured number of retries the last error is thrown as inner exception.
+ *
+ * When you do not want to throw an exception but return a default value instead you can specify the `resolveOnTimeout` option.
+ * When this is ser with a value the function will return the specified value when the function still fails after the configured number of retries.
+ *
+ * @example
+ * ```typescript
+ * class MyClass {
+ *   @retryable({ maxRetries: 3, interval: 500 })
+ *   async myMethod() {
+ *     // do something that might fail
+ *   }
+ * }
+ * ```
+ * @param options An optional options object to configure the retry behavior
+ * @returns MethodDecorator
+ */
+export function retryable<T extends (...args: TArgs[]) => Promise<TReturn>, TArgs = any, TReturn = any>(options?: RetryableOptions): MethodDecorator {
+    return function<K = T>(target: any, name: string | symbol, descriptor: TypedPropertyDescriptor<K>): TypedPropertyDescriptor<K> | void {
+        if (!descriptor.value || typeof descriptor.value !== 'function') {
+            return;
+        }
+
+        return {
+            ...descriptor,
+            value: makeRetryable(descriptor.value.bind(target), options) as any
+        };
+    };
+}
+
+/**
+ * @see retryable
+ * @param originalMethod Method to make retryable
+ * @param options An optional options object to configure the retry behavior
+ * @returns 
+ */
+export function makeRetryable<T extends (...args: Parameters<T>) => Promise<Awaited<ReturnType<T>>>>(originalMethod: T, options?: RetryableOptions): (...args: Parameters<T>) => Promise<Awaited<ReturnType<T>>> {
+    return async function(...args: Parameters<T>) : Promise<Awaited<ReturnType<T>>> {
+        let attempt = 0;
+        let lastError: any;
+
+        while (attempt++ < (options?.maxRetries ?? 3)) {
+            try {
+                const result = await originalMethod(...args);
+                return result;
+            } catch(err) {
+                lastError = err;
+                await wait(options?.interval ?? 500);
+            }
+        }
+
+        if (options && 'resolveOnTimeout' in options) {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            return options.resolveOnTimeout as any;
+        }
+
+        throw new CustomError(
+            options?.rejectionMessage ?? `Retry failed after ${attempt} attempts`, 
+            {
+                name: 'RETRY_FAILED', 
+                innerError: lastError 
+            }
+        );
+    }
 }
