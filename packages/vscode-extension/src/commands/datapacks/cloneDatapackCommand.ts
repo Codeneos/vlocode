fbd723da3fcdae3625808e31cf62f70ef10cde39
Peter van Gulik <peter@curlybracket.nl>
2023-08-20 16:44:36 +0200

    fix: datapack rename and close causes name duplication when the old name is present in the old name; rename and clone now also take into account other global keys such on product2 datapacks.

--- packages/vscode-extension/src/commands/datapacks/cloneDatapackCommand.ts
+++ packages/vscode-extension/src/commands/datapacks/cloneDatapackCommand.ts
@@ -6,6 +6,8 @@ import { VlocityDatapack } from '@vlocode/vlocity';
 
 import * as vscode from 'vscode';
 import { DatapackCommand } from './datapackCommand';
+import { segregate } from '@vlocode/util';
+import { randomUUID } from 'crypto';
 
 @vscodeCommand(VlocodeCommand.cloneDatapack)
 export default class CloneDatapackCommand extends DatapackCommand {
@@ -18,15 +20,19 @@ export default class CloneDatapackCommand extends DatapackCommand {
         return this.cloneDatapacks(args[1] || [args[0] || this.currentOpenDocument]);
     }
 
-    protected async cloneDatapacks(selectedFiles: vscode.Uri[]) : Promise<any> {        
+    protected async cloneDatapacks(selectedFiles: vscode.Uri[]) : Promise<any> {
         const datapacks = await this.vlocode.withProgress('Loading datapack...', () => this.loadDatapacks(selectedFiles));
         for (const datapack of datapacks) {
-            await this.cloneDatapack(datapack, { updateGlobalKey: true });  
+            const datapackName = datapack.name || datapack.sourceKey;
+            await this.cloneDatapack(datapack, { updateGlobalKey: true });
+            const clonedDatapackName = datapack.name || datapack.sourceKey;
+            vscode.window.showInformationMessage(`Cloned datapack ${datapackName} as ${clonedDatapackName}`);
         }
     }
 
     protected async cloneDatapack(datapack: VlocityDatapack, options?: { updateGlobalKey?: boolean }) : Promise<any> {
         const matchingFields = await this.exportQueries.getMatchingFields(datapack.datapackType);
+        const datapackName = datapack.name || datapack.sourceKey;
 
         if (!matchingFields) {
             return vscode.window.showWarningMessage('The selected datapack does not have any matching fields');
@@ -37,17 +43,16 @@ export default class CloneDatapackCommand extends DatapackCommand {
         }
 
         for (const field of matchingFields) {
-            const value = await vscode.window.showInputBox({
-                prompt: `Enter ${field}...`,
-                ignoreFocusOut: true,
-                value: datapack[field]
-            });  
-            
-            if (value === undefined) {
-                return;
-            }            
-  
+            if (/Global[a-z]*Key__c$/i.test(field)) {
+                continue;
+            }
+            const currentValue = datapack[field];
+            const value = await this.getMatchingKeyFieldValue(field, currentValue);
+
             if (field === 'Name') {
+                if (typeof value !== 'string') {
+                    throw new Error('The datapack name field must be a string');
+                }
                 datapack.rename(value);
             } else {
                 datapack.updateField(field, value);
@@ -56,10 +61,45 @@ export default class CloneDatapackCommand extends DatapackCommand {
 
         // Regen global keys
         if (options?.updateGlobalKey) {
-            datapack.regenerateGlobalKey();
+            datapack.updateGlobalKeyField();
         }
 
         // execute rename and expand into new folder structure
-        await this.vlocode.withActivity(`Clone datapack ${datapack.name}...`, () => this.vlocode.datapackService.expandDatapack(datapack, datapack.projectFolder));
+        await this.vlocode.withActivity(
+            `Clone datapack ${datapackName}...`,
+            async () => {
+                await this.vlocode.datapackService.expandDatapack(datapack, datapack.projectFolder);
+            }
+        );
+    }
+
+    private async getMatchingKeyFieldValue(field: string, currentValue: any) {
+        const newValue = await vscode.window.showInputBox({
+            prompt: `Enter new value for: ${field}`,
+            ignoreFocusOut: true,
+            value: `${currentValue}`
+        });
+
+        if (newValue === undefined) {
+            throw new Error(`Operation cancelled, no value provided for: ${field}`);
+        }
+
+        if (typeof currentValue === 'number') {
+            const numberValue = Number(newValue);
+            return isNaN(numberValue) ? newValue : numberValue;
+        }
+
+        if (typeof currentValue === 'boolean') {
+            if (/true|yes|1/i.test(newValue)) {
+                return true;
+            } else if (/false|no|0/i.test(newValue)) {
+                return false;
+            }
+            throw new Error(`${field} requires a boolean value, "${
+                    newValue
+                }" cannot be converted to a boolean. Use "true" or "false" instead.`);
+        }
+
+        return newValue;
     }
 }
