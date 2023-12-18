2deeaffb47751043f3e60211bc27765679cc90ee
Peter van Gulik <peter@curlybracket.nl>
2023-12-18 15:42:08 +0100

    feat: support new execute API from codelens

--- packages/vscode-extension/src/codeLensProviders/executeApiLensProvider.ts
+++ packages/vscode-extension/src/codeLensProviders/executeApiLensProvider.ts
@@ -2,16 +2,21 @@ import * as vscode from 'vscode';
 
 import VlocodeService from "../lib/vlocodeService";
 import { container, injectable } from "@vlocode/core";
+import { VlocodeCommand } from '../constants';
 
 @injectable()
 export class ExecuteApiLensProvider implements vscode.CodeLensProvider {
+
+    private documentFilter : Array<vscode.DocumentFilter> = [
+        { pattern: '**/*.{api,http,sfhttp,sfapi}' },
+        { language: 'sfhttp' }
+    ];
 
     private regex = /^(GET|POST|PUT|DELETE|PATCH) (.*)$/i
 
     public static register(service: VlocodeService) {
         const lens = container.get(ExecuteApiLensProvider);
-        vscode.languages.registerCodeLensProvider({ pattern: '**/*.{api,http,sfhttp,sfapi}' }, lens);
-        vscode.languages.registerCodeLensProvider({ language: 'sfhttp' }, lens);
+        vscode.languages.registerCodeLensProvider(lens.documentFilter, lens);
     }
 
     public provideCodeLenses(document: vscode.TextDocument): vscode.CodeLens[] {
@@ -22,9 +27,9 @@ export class ExecuteApiLensProvider implements vscode.CodeLensProvider {
                 return [
                     new vscode.CodeLens(line.range, {
                         title: `Execute as Salesforce API Request (${match[1].toLowerCase()})`,
-                        tooltip: "Execute the API request described in this file on the selected Salesforce org",
-                        command: "vlocode.api.execute",
-                        arguments: [ document ]
+                        tooltip: 'Execute the API request described in this file on the selected Salesforce org',
+                        command: VlocodeCommand.execRestApi,
+                        arguments: [ document.uri ]
                     })
                 ];
             } else if (line.text) {
@@ -32,5 +37,9 @@ export class ExecuteApiLensProvider implements vscode.CodeLensProvider {
             }
         }
         return [];
+    }
+
+    public resolveCodeLens(codeLens: vscode.CodeLens, token: vscode.CancellationToken): vscode.CodeLens | undefined {
+        return codeLens;
     }
 }
