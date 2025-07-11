ccd80b054936f9e71202110a814794b84824dd09
Peter van Gulik <peter@curlybracket.nl>
2025-07-11 14:55:27 +0200

    feat: load override matching keys from `matching-keys.json` when present. Matching keys defined in this file always overrule matching keys defined in the org.

--- packages/vlocity-deploy/src/deploymentSpecs/matchingFields.ts
+++ packages/vlocity-deploy/src/deploymentSpecs/matchingFields.ts
@@ -4,35 +4,43 @@ import { DatapackDeploymentRecord } from '../datapackDeploymentRecord';
 import { FileSystem, Logger } from '@vlocode/core';
 import { asArray } from '@vlocode/util';
 
+type MatchingKeysCollectionType = 'defaults' | 'overrides';
+type MatchingKeysCollection = Record<string, string[]>;
+
 @deploymentSpec({ recordFilter: /.*/ })
 export class MatchingFieldsSpec implements DatapackDeploymentSpec {
 
-    private matchingKeyFieldsPath = 'matching-keys.json';
-    private defaultsInitialized = false;
+    private overridesFiles = 'matching-keys.json';
+    private initialized = false;
 
-    private upsertFields = {
-        OmniDataTransformItem: [
-            // Purge old mappings before adding new ones
-        ],
-        OmniDataTransform: [
-            'VersionNumber',
-            'Name',
-        ],
-        OmniUiCard: [
-            'VersionNumber',
-            'Name',
-        ],
-        CustomObjectMap__c: [
-            '%vlocity_namespace%__DestinationParentObject__c',
-            '%vlocity_namespace%__SourceParentObject__c',
-        ],
-        CustomFieldMap__c: [
-            "%vlocity_namespace%__DestinationFieldName__c",
-            "%vlocity_namespace%__DestinationSObjectType__c",
-            "%vlocity_namespace%__SourceFieldName__c",
-            "%vlocity_namespace%__SourceSObjectType__c",
-        ]
-    }
+    private matchingKeyFields: Record<MatchingKeysCollectionType, MatchingKeysCollection>  = {
+        overrides: {
+            // Loaded from the matching-keys.json file
+        },
+        defaults: {
+            OmniDataTransformItem: [
+                // Purge old mappings before adding new ones
+            ],
+            OmniDataTransform: [
+                'VersionNumber',
+                'Name',
+            ],
+            OmniUiCard: [
+                'VersionNumber',
+                'Name',
+            ],
+            CustomObjectMap__c: [
+                '%vlocity_namespace%__DestinationParentObject__c',
+                '%vlocity_namespace%__SourceParentObject__c',
+            ],
+            CustomFieldMap__c: [
+                "%vlocity_namespace%__DestinationFieldName__c",
+                "%vlocity_namespace%__DestinationSObjectType__c",
+                "%vlocity_namespace%__SourceFieldName__c",
+                "%vlocity_namespace%__SourceSObjectType__c",
+            ]
+        } 
+    };
 
     constructor(
         private readonly fs: FileSystem,
@@ -45,37 +53,62 @@ export class MatchingFieldsSpec implements DatapackDeploymentSpec {
         await this.initializeMatchingKeys();
 
         // Ensure that all records have upsert fields set, either from the defaults or from the record itself
-        for (const record of records) {
-            if (record.upsertFields?.length) {
-                continue;
-            }
-            if (record.normalizedSObjectType in this.upsertFields) {
-                record.upsertFields = [...this.upsertFields[record.normalizedSObjectType]];
+        for (const record of records) {     
+            const normalizedType = this.normalizedType(record.normalizedSObjectType);
+            if (normalizedType in this.matchingKeyFields.overrides) {
+                // Overrides always replace the upsert fields for the record
+                record.upsertFields = [...this.matchingKeyFields.overrides[normalizedType]];
+            } else if (normalizedType in this.matchingKeyFields.defaults) {
+                // If there are no upsert fields set, use the defaults for the record type if set
+                if (!record.upsertFields || record.upsertFields.length === 0) {
+                    record.upsertFields = [...this.matchingKeyFields.defaults[normalizedType]];
+                }
             }
         }
     }
 
     private async initializeMatchingKeys() {
-        if (this.defaultsInitialized) {
+        if (this.initialized) {
             return;
         }
+        this.matchingKeyFields = {
+            defaults: this.normalizeMatchingKeys(this.matchingKeyFields.defaults),
+            overrides: await this.loadOverrides(this.overridesFiles)
+        };
+        this.initialized = true;
+    }
 
-        this.defaultsInitialized = true;
-        if (!await this.fs.pathExists(this.matchingKeyFieldsPath)) {
-            return;
+     private async loadOverrides(path: string) {        
+        if (!await this.fs.pathExists(path)) {
+            return {};
         }
-
+        const matchingKeys: MatchingKeysCollection = {};
         try {
             // Load the matching key fields from the file
-            this.logger.verbose('Loading default matching key fields from: %s', this.matchingKeyFieldsPath);
-            const matchingKeyFields = JSON.parse(await this.fs.readFileAsString(this.matchingKeyFieldsPath));
+            this.logger.verbose('Override matching key fields from: %s', path);
+            const matchingKeyFields = JSON.parse(await this.fs.readFileAsString(path));
             for (const [sObjectType, fields] of Object.entries(matchingKeyFields)) {
-                const normalizedType = sObjectType.replace('%vlocity_namespace%__', '');
-                this.upsertFields[normalizedType] = asArray(fields).map(field => String(field));
-                this.logger.verbose(`Set default matching key fields for %s: %s`, normalizedType, this.upsertFields[normalizedType]);
+                const normalizedType = this.normalizedType(sObjectType);
+                matchingKeys[normalizedType] = asArray(fields).map(field => String(field));
+                this.logger.verbose(`Override matching key fields for %s: %s`, normalizedType, matchingKeys[normalizedType].join(', '));
             }
         } catch (error) {
             this.logger.warn(`Failed to load matching key fields from supplement file: %s`, error);
         } 
+        return matchingKeys;
+    }
+
+    private normalizeMatchingKeys(matchingKeys: MatchingKeysCollection): MatchingKeysCollection {
+        // Normalize the matching key fields to ensure they are all in the same format
+        const normalizedKeys: MatchingKeysCollection = {};
+        for (const [sObjectType, fields] of Object.entries(matchingKeys)) {
+            const normalizedType = this.normalizedType(sObjectType);
+            normalizedKeys[normalizedType] = fields;
+        }
+        return normalizedKeys;
+    }
+
+    private normalizedType(sObjectType: string): string {
+        return sObjectType.replace('%vlocity_namespace%__', '').toLowerCase().trim();
     }
 }
