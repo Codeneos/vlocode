595bba77a2f346874da9b92ecb5ad1cb6dd7231c
Peter van Gulik <peter@curlybracket.nl>
2025-08-15 21:20:17 +0200

    chore: copy decomposed metadata presets from SF

---
+++ packages/salesforce/src/registry/presets/index.ts
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { MetadataRegistry } from '../types';
+
+// we have to import all presets explicitly for VSCE's esbuild bundling process
+// other read methods might make esbuild fail to bundle the json file
+import * as decomposeCustomLabelsBeta2 from './decomposeCustomLabelsBeta2.json';
+import * as decomposePermissionSetBeta2 from './decomposePermissionSetBeta2.json';
+import * as decomposeSharingRulesBeta from './decomposeSharingRulesBeta.json';
+import * as decomposeWorkflowBeta from './decomposeWorkflowBeta.json';
+import * as decomposeExternalServiceRegistrationBeta from './decomposeExternalServiceRegistrationBeta.json';
+
+export const presetMap = new Map<string, MetadataRegistry>([
+  ['decomposeCustomLabelsBeta2', decomposeCustomLabelsBeta2 as MetadataRegistry],
+  ['decomposePermissionSetBeta2', decomposePermissionSetBeta2 as MetadataRegistry],
+  ['decomposeSharingRulesBeta', decomposeSharingRulesBeta as MetadataRegistry],
+  ['decomposeWorkflowBeta', decomposeWorkflowBeta as MetadataRegistry],
+  ['decomposeExternalServiceRegistrationBeta', decomposeExternalServiceRegistrationBeta as MetadataRegistry],
+]);
